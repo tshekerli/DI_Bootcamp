b9d168276f3c552960c57b6889c4d4835ad59cdd
tshekerli <tshekerli@gmail.com>
2023-12-10 23:07:05 +0400

    Fix bug in login functionality

--- W3/Day1/script.js
+++ W3/Day1/script.js
@@ -1,127 +1,203 @@
-// console.log('Hello World');
-// console.log('This is day 1 of Javascript');
+//console.log
+// console.log("Hello World");
+// console.log("This is day 1 of javascript");
 
-// // Variables
-
+//Variables
 // let x = 3;
-
 // console.log(x);
 
-// // Naming
-// All JavaScript variables must be identified with unique names.
-// These unique names are called identifiers.
+// let day1="now"
 
-// The general rules for constructing names for variables (unique identifiers) are:
+//camelcase
+// let dayOfTheMonth = 10
 
-// Names can contain letters, digits, underscores, and dollar signs.You cannot use any other characters, including spaces, symbols, and punctuation marks.
-// Names must begin with a letter
-// Names can also begin with $ and _
-// Names are case sensitive (y and Y are different variables)
-// You cannot use one of JavaScript’s reserved words as a variable name. All programming languages have a supply of words that are used internally by the language and that can’t be used for variable names because doing so would cause confusion
-// JavaScript’s keywords
-// JavaScript’s reserved words
-// Make your names descriptive.
-// When the name contains multiple words, camelCase is commonly used. That is: words go one after another, each word except first starting with a capital letter: myVeryLongName.
-
-// Examples of valid names:
-
-// let userName;
-// let test123;
-// Examples of incorrect variable names:
-
-// let 1a; // cannot start with a digit
-// let my-name; // hyphens '-' aren't allowed in the name
-
-// Keywords
-// The let keyword tells the browser to create variables:
-
+//Keywords
 // let x, y;
+
+// let x = 5 + 6;
 // x = 5 + 6;
+// console.log(x);
+
 // y = x * 10;
+// let y = x * 10;
+// console.log(y);
 
-// Comments
-// Code after double slashes // or between /* and */is treated as a comment.
-// Comments are ignored, and will not be executed:
+//DataTypes
+//Strings
+// console.log("Hello");
+// console.log('Hello');
 
-// let x = 5;   // I will be executed
-// // let x = 6;   I will NOT be executed
+// let a = "Hello";
+// let b = "World";
+// let c = a + " " + b;
+// console.log(c);
 
-// Undefined
-// Undefined is when a variable is declared but not assigned to anything.
-// let a;
-// You have created the variable a, but it has no assignment and is undefined.
+// console.log(
+//   "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur \
+//   consequuntur ad, ratione quidem temporibus veniam assumenda debitis fugiat ipsum, in voluptatibus, minima nulla quasi sed modi! Quos aut amet atque ipsam, error, dolore libero qui aliquid, tempora fugiat deleniti numquam dolorum vitae architecto repudiandae voluptatibus harum. Dolores laborum a, tempore cumque ipsum consequatur accusamus ipsa molestiae asperiores  atque distinctio vel ullam quas dignissimos debitis, eaque nulla natus reprehenderit! Laborum, non beatae sint facere vel ipsa eveniet veniam! Pariatur voluptas repellat eveniet animi dolore minima deserunt. Deserunt quaerat omnis qui eos quidem, ipsa quas ex corrupti eaque vero libero corporis exercitationem?"
+// );
 
-// Javascript Primitive Data Types
+//String Properties
 
-// String
-// This is the text that we put inside quotes: "Hello world"
+// let longString =
+//   "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur consequuntur ad, ratione quidem temporibus veniam assumenda debitis fugiat ipsum, in voluptatibus, minima nulla quasi sed modi! Quos aut amet atque ipsam, error, dolore libero qui aliquid, tempora fugiat deleniti numquam dolorum vitae architecto repudiandae voluptatibus harum. Dolores laborum a, tempore cumque ipsum consequatur accusamus ipsa molestiae asperiores  atque distinctio vel ullam quas dignissimos debitis, eaque nulla natus reprehenderit! Laborum, non beatae sint facere vel ipsa eveniet veniam! Pariatur voluptas repellat eveniet animi dolore minima deserunt. Deserunt quaerat omnis qui eos quidem, ipsa quas ex corrupti eaque vero libero corporis exercitationem?";
 
-// Strings may have single quotes ('), double quotes ("), or backticks (`) which are read equally, but they must have the exact, consistent quotes at beginning and end.
+// let longStringLength = longString.length;
+// console.log(longStringLength);
 
-// To use an apostrophe within a string, either uses double quotes around the string or escape the apostrophe with a backslash immediately before it. Like so: 'Jimmy\'s Car'
+// let smallerString = "Hello , welcome to Javascript Day1";
+// let smallerStringLength = smallerString.length;
+// console.log(smallerString.length);
 
-// When you add two strings (or even a string and a number), JS will perform string concatenation and return the two strings together:
+//String Methods
+// let smallerString = "Hello , welcome to Javascript Day1";
+// console.log(smallerString[34]);
 
-//     let a = "Hello";
-//     let b = "World";
-//     let c = a+b;
-//     console.log(c);
-// The console would show: "HelloWorld".
-
-// Anything inside the quotes is considered a string, even numbers: “43” (string) ≠ 43 (number).
-
-// Long Literal Strings
-// Rather than having lines that go on endlessly, you can specifically break the string into multiple lines in
-// the source code without affecting the actual string contents.
-
-// The + operator
-
-// let longString = "This is a very long string which needs " +
-//                  "to wrap across multiple lines because " +
-//                  "otherwise, my code is unreadable.";
-// The backslash character \
-
-// At the end of each line to indicate that the string will continue on the next line.
-// Make sure there is no space or any other character after the backslash.
-
-// let longString = "This is a very long string which needs \
-// to wrap across multiple lines because \
-// otherwise my code is unreadable.";
-
-// String Properties
-// Built-in length property:
-// let lengthTxt = longString.length
-
-// String Methods
-// The indexOf() method: returns the index of (the position of) the first occurrence of a specified text in a string:
-// Return -1 if the text is not found
-// let str = "Hello Everyone, please say hello to the class";
-// let posUppercaseHello = str.indexOf("Hello"); // case sensitive
+// let str = "Hello Everyone, please say hello to the class.";
+// let posUppercaseHello = str.indexOf("Hello");
 // let posLowercaseHello = str.indexOf("hello");
-// console.log(posUppercaseHello) // 0
-// console.log(posLowercaseHello) // 27
+// console.log("Hello Everyone, please say hello to the class.");
+// console.log(posUppercaseHello);
+// console.log(posLowercaseHello);
 
-// let secondstr = "hello Everyone, please say hello to the class";
-// let secondStrPosHello = secondstr.indexOf("hello");
-// console.log(secondStrPosHello) // 0
-// The substring(startIndex, endIndex) methods:
-// Returns the characters in a string that begins at “startIndex” and ends at the “endIndex”.
-// “endIndex” is optional, and if omitted, up to the end of the string is assumed.
-// “endIndex” is not included.
-// let str = "Hello Everyone, please say hello to the class ";
-// str.substring(0,4) //returns "Hell" --> the index 4 isn't included
-// str.substring(2,4) //returns "ll"
-// str.substring(15) //returns "please say hello to the class"
-// The toLowerCase() method: Returns the string with all of its characters converted to lowercase.
-// let myStr = "Hello Everyone, Please say hello to the class ";
-// myStr.toLowerCase() //returns "hello everyone, please say hello to the class "
+// console.log(str.substring(0, 5));
 
-// More String Methods
-// replace()
-// concat()
-// toUpperCase()
-// trim()
-// charAt()
+// console.log(str.substring(15));
 
-let x = 5 + 6;
-console.log(x);
+// let uppercaseString = "HELLO THIS IS AN INTENSE STRING";
+// // console.log(uppercaseString.length)
+// console.log(uppercaseString.toLowerCase());
+// let addressNumber = 19;
+// console.log(addressNumber);
+// let addressStreet = "Nitzana";
+// let country = "Israel";
+
+// let globalAddress = "I live in " + addressNumber + " " + addressStreet + " " + country;
+// console.log(globalAddress);
+
+//Numbers
+
+// console.log("1" + 2);
+
+// console.log(1 + 2);
+
+// console.log(1 - 2);
+
+// console.log(1 / 2);
+
+// console.log(1 * 2);
+
+//Number Methods
+
+// let op = "me";
+// console.log(isNaN(op));
+
+// let num = 1;
+// console.log(isNaN(num));
+// isNaN(op);
+
+// let op = 10.6789;
+
+// console.log(op.toFixed(3));
+
+// let op = 10;
+// console.log(op + 1);
+// console.log(op.toString() + 1);
+
+// console.log(typeof op);
+
+//Exercise 2
+// let birthYear = 1997;
+// let futureYear = 2025;
+// let age = futureYear - birthYear;
+// console.log("I will be " + (futureYear - birthYear) + " in " + futureYear);
+
+//Boolean
+
+// console.log(Boolean(typeof birthYear === "string"));
+
+//Arrays
+// let user1 = "John";
+// let user2 = "Bill";
+// let user3 = "Nancy";
+
+// let users = ["John", "Bill", "Nancy"];
+// console.log(users[0]);
+// console.log(users[1]);
+// console.log(users[2]);
+// console.log(users[3]);
+
+// let nestedArrays = [
+//   [1, 2, 3],
+//   [4, "hello", 6],
+//   [7, 8, 9],
+// ];
+// console.log(nestedArrays);
+// console.log(nestedArrays[1][1]);
+
+// console.log(nestedArrays.length);
+
+//Changing an element in an array:
+// let colors = ["blue", "yellow", "green"];
+// console.log(colors);
+// colors[1] = "pink";
+// console.log(colors);
+
+//Array methods:
+//Push and Pop
+// let colors = ["blue", "yellow", "green"];
+// console.log(colors);
+// colors.push("orange");
+// colors.push("red");
+// console.log(colors);
+// colors.pop();
+// console.log(colors);
+
+//Splice
+
+// let colors = ["blue", "yellow", "green"];
+// colors.splice(1, 1, "pink", "white", "black");
+// console.log(colors);
+
+// colors.splice(1, 0, "pink", "grey");
+// console.log(colors);
+
+//Slice-> Creates a new array
+// let colors = ["blue", "yellow", "green", "pink"];
+
+// let favorite = colors.slice(2);
+// console.log(favorite);
+// console.log(colors);
+
+// let favorite2 = colors.slice(1, 3);
+// console.log(favorite2);
+
+//tostring
+// let colorstring = colors.toString();
+// console.log(colors);
+// console.log(colorstring);
+
+//Exercise3
+// let pets = ["cat", "dog", "fish", "rabbit", "cow"];
+// console.log(pets[1]);
+
+// pets.splice(3, 1, "horse");
+// console.log(pets);
+// console.log(pets.length);
+
+//User Interface Functions
+//1 Alert
+// console.log("before alert");
+// alert("You are not authorized to enter");
+// console.log("after alert");
+
+//2 confirm
+
+// let userAnswer = confirm("Are you over 18?");
+// console.log(userAnswer);
+
+//3 prompt
+
+// let userAnswer = prompt("What is your username?");
+
+// console.log("Welcome to the website " + userAnswer);
