b4ffda7da664f9efd883e5228edd2858405da63b
tshekerli <tshekerli@gmail.com>
2023-12-27 20:16:33 +0400

    ExerciseXp Done

--- W5/Day4/ExerciseXp/script.js
+++ W5/Day4/ExerciseXp/script.js
@@ -38,25 +38,53 @@
 // Copy the code above, to a structured HTML file.
 // In your Javascript file, use setInterval to move the <div id="animate"> to the right side of the <div id="container">, when the button is clicked on.
 
-let button = document.getElementById("animate");
-// The <div id="animate"> should move 1px to the right every milisecond, until it reaches the end of the <div id="container">.
+// let button = document.getElementById("animate");
+// // The <div id="animate"> should move 1px to the right every milisecond, until it reaches the end of the <div id="container">.
 
-function myMove() {
-    let animate = document.getElementById("animate");
-    let container = document.getElementById("container");
-    let position = 0;
-    let id = setInterval(frame, 1);
+// function myMove() {
+//     let animate = document.getElementById("animate");
+//     let container = document.getElementById("container");
+//     let position = 0;
+//     let id = setInterval(frame, 1);
 
-    function frame() {
-        if (position == 350) {
-            clearInterval(id);
-        } else {
-            position++;
-            animate.style.left = position + "px";
-        }
-    }
-}
+//     function frame() {
+//         if (position == 350) {
+//             clearInterval(id);
+//         } else {
+//             position++;
+//             animate.style.left = position + "px";
+//         }
+//     }
+// }
 // Hint : use clearInterval as soon as the box reaches the right end side of the container
 
 
 // Hint : check out the demonstration in the Course Noted named JS Functions
+
+
+// In your Javascript file add the functionality which will allow you to drag the box and drop it into the target. Check out the Course Notes named DOM animations.
+
+
+let box = document.getElementById('box');
+let isDown = false;
+let offset = [0, 0];
+
+box.addEventListener('mousedown', function(e) {
+    isDown = true;
+    offset = [
+        box.offsetLeft - e.clientX,
+        box.offsetTop - e.clientY
+    ];
+}, true);
+
+document.addEventListener('mouseup', function() {
+    isDown = false;
+}, true);
+
+document.addEventListener('mousemove', function(event) {
+    event.preventDefault();
+    if (isDown) {
+        box.style.left = (event.clientX + offset[0]) + 'px';
+        box.style.top  = (event.clientY + offset[1]) + 'px';
+    }
+}, true);
