de6709c641c71d0af7fc7f9cc3ec989847d84741
tshekerli <tshekerli@gmail.com>
2023-12-28 20:24:52 +0400

    Refactor variable names for clarity

---
+++ W5/Day3/Drag&Drop/script.js
@@ -0,0 +1,9 @@
+function onDragStart4PinkSquare(event){
+    console.log("start");}
+
+    function onDragEnd4PinkSquare(event){
+        console.log("end");}
+
+function onDrag4PinkSquare(event){
+    console.log(event.clientX, event.clientY);
+}
