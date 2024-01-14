04cf8cc144f18b1d19a5be3c126af87d4d450619
tshekerli <tshekerli@gmail.com>
2024-01-14 14:55:09 +0400

    ExerciseXpGold done

--- W7/Day6/ExerciseXpGold/script.js
+++ W7/Day6/ExerciseXpGold/script.js
@@ -35,18 +35,18 @@
 // Write a JavaScript function to remove: null, 0, "", false, undefined and NaN values from an array.
 // Sample array : [NaN, 0, 15, false, -22, '',undefined, 47, null]
 // Expected result : [15, -22, 47]
-const array = [NaN, 0, 15, false, -22, '', undefined, 47, null];
-const removeValues = (x) => {
-    let newArray = [];
-    for (let i = 0; i < x.length; i++) {
-        if (x[i]) {
-            newArray.push(x[i])
-        }
-    }
-    return newArray
-}
+// const array = [NaN, 0, 15, false, -22, '', undefined, 47, null];
+// const removeValues = (x) => {
+//     let newArray = [];
+//     for (let i = 0; i < x.length; i++) {
+//         if (x[i]) {
+//             newArray.push(x[i])
+//         }
+//     }
+//     return newArray
+// }
 
-console.log(removeValues(array));
+// console.log(removeValues(array));
 
 // Exercise 4 : Repeat Please !
 // Instructions
@@ -55,23 +55,36 @@ console.log(removeValues(array));
 // console.log(repeat('Ha!',3));
 // "Ha!Ha!Ha!"
 
+// const repeat = (str, num) => {
+//     let newStr = "";
+//     for (let i = 0; i < num; i++) {
+//         newStr += str
+//     }
+//     return newStr
+// }
+
+// console.log(repeat('Ha!', 3));
 
 // Exercise 5 : Turtle & Rabbit
 // For this exercise, look at the lesson More JS methods.
 
 // Using this code :
 
-// const startLine = '     ||<- Start line';
-// let turtle = '🐢';
-// let rabbit = '🐇';
+const startLine = '     ||<- Start line';
+let turtle = '🐢';
+let rabbit = '🐇';
 // Line up the Turtle and the Rabbit at the start line.
 // Expected Output:
+let pad = startLine.indexOf('-');
+turtle = turtle.padStart(pad);
+rabbit = rabbit.padStart(pad);
+turtle = turtle.trim().padEnd(9, '='); 
 
 //     When you write:
 
-//     console.log(startLine);
-//     console.log(turtle);
-//     console.log(rabbit);
+    console.log(startLine);
+    console.log(turtle);
+    console.log(rabbit);
 
 //     It should look like this:
 
@@ -80,4 +93,10 @@ console.log(removeValues(array));
 //     '       🐇'
 
 
+
+
+
 // What happens when you run turtle = turtle.trim().padEnd(9, '='); ?
+// It removes the white space from the turtle and adds 6 = signs to the end of the string. The reason why the turtle has 6 equal signs after it instead of 9 is because of the padEnd() method used in the code.
+
+// The padEnd() method is used to pad the end of a string with a specified character until it reaches a desired length. In this case, the desired length is 9 characters.
