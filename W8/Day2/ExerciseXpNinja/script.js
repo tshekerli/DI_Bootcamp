33a71f36a013829ac6bf861c4637528fe3b54a3b
tshekerli <tshekerli@gmail.com>
2024-01-18 16:31:13 +0400

    Refactor mergeWords function to use currying

---
+++ W8/Day2/ExerciseXpNinja/script.js
@@ -0,0 +1,27 @@
+// Exercise 1 : Merge Words
+// Instructions
+
+//     Create a function such as mergeWords('Hello')() that returns the following string: 'Hello'
+
+//     When the function is called without any arguments return a string where all words have been merged into a sentence.
+
+//     For example
+    // mergeWords('There')('is')('no')('spoon.')();
+    // should return 'There is no spoon.'
+
+    // Below is a verbose JavaScript solution, turn this into a currying function.
+
+    // function mergeWords(string) {
+    //  return function(nextString) {
+    //    if (nextString === undefined) {
+    //      return string;
+    //    } else {
+    //      return mergeWords(string + ' ' + nextString);
+    //    }
+    //  }
+    // }
+
+    
+const mergeWords = (string) => (nextString) => nextString === undefined ? string : mergeWords(string + ' ' + nextString);
+
+console.log(mergeWords('There')('is')('no')('spoon.')());
