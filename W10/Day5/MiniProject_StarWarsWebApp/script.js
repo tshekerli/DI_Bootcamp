ccf1b5d2362d47791c6b2cc1efc8cbbb6eb547ff
tshekerli <tshekerli@gmail.com>
2024-02-01 20:51:59 +0400

    Add Star Wars web app functionality and update styling

---
+++ W10/Day5/MiniProject_StarWarsWebApp/script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('#button');
+const name = document.querySelector('.name');
+const height = document.querySelector('.height');
+const gender = document.querySelector('.gender');
+const birthYear = document.querySelector('.birth-year');
+const homeworld = document.querySelector('.homeworld');
