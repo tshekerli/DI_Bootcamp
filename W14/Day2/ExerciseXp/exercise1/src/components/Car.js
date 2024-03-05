8b7a0bb540c96105393a70d4733c68b0207154b1
tshekerli <tshekerli@gmail.com>
2024-03-05 14:01:17 +0400

    Delete unnecessary files and code

--- W14/Day2/ExerciseXp/exercise1/src/components/Car.js
+++ W14/Day2/ExerciseXp/exercise1/src/components/Car.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
+import Garage from './Garage';
 
 function CarInfo(object){
+    
     const [color, setColor] = useState('red'); 
 
     return(
         <div>
             <header>This car is {color} {object.model}</header>
             <button onClick={() => setColor('blue')}>Change color</button>
+            <Garage size="small" />
         </div>
     )
 }
