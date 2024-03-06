de11ffb876e3108dfc582fcdd82d39b4242013cb
tshekerli <tshekerli@gmail.com>
2024-03-06 12:23:12 +0400

    DailyChallenge done

---
+++ W14/Day4/dailychallenge/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import FormContainer from "./FormContainer";
+
+import "./App.css"
+
+function App() {
+  return <FormContainer />;
+}
+export default App;
