bd3b7bbdd61f3a16ee9ed2f69bf688b1e2099ebb
tshekerli <tshekerli@gmail.com>
2024-03-10 20:20:27 +0400

    Fix bug in login functionality

---
+++ W15/Day5/config/config.js
@@ -0,0 +1,26 @@
+import knex from "knex";
+
+
+const config = {
+    client: "pg",
+    connection: {
+        host: "localhost",
+        port: 5432,
+        user:  "postgres",
+        password: "12345",
+        database: "miniproject"
+    }
+};
+
+export const db = knex(config);
+db.schema.createTable("todo", (table) => {
+    table.increments("id").primary();
+    table.string("title");
+    table.boolean("completed");
+}).then(() => {
+    console.log("table created");
+}).catch((err) => {
+    console.log(err);
+}).finally(() => {
+    db.destroy();
+});
