8e587a9682d52a8d6ebb79cd5eb1c1f20d008431
tshekerli <tshekerli@gmail.com>
2024-03-31 20:18:57 +0400

    Update function to handle edge case scenarios

---
+++ W19/Day1/main.py
@@ -0,0 +1,3 @@
+futuretime = 123879
+my_age = 29
+print("I will be " + str(my_age + futuretime) +" years old"+ " in" + " " + str(futuretime) + " years")
---
+++ W19/Day2/ExerciseXp/main.py
@@ -0,0 +1,5 @@
+# Exercise 1
+# how can I print hello worldd 4 times in a single line?
+
+print("Hello World! Hello World! Hello World! Hello World!")
+
