9906b55ce080ac3249d070901e841209086a04d2
tshekerli <tshekerli@gmail.com>
2024-03-31 20:41:30 +0400

    Refactor code and add exercise solutions

--- W19/Day2/ExerciseXp/main.py
+++ W19/Day2/ExerciseXp/main.py
@@ -1,5 +1,76 @@
 # Exercise 1
-# how can I print hello worldd 4 times in a single line?
 
-print("Hello World! Hello World! Hello World! Hello World!")
 
+# print("Hello World! \nHello World!\nHello World!\nHello World!")
+
+
+# Exercise 2
+
+# print((99**3)*8)
+
+# Exercise 3
+# >>> 5 < 3  guess: false answer false
+ 
+# >>> 3 == 3 guess: true answer true
+
+# >>> 3 == "3" guess: false answer false
+
+# >>> "3" > 3 guess: type error, answer type error
+
+# >>> "Hello" == "hello" guess: false answer false
+
+# print(5 < 3)
+# print(3 == 3)
+# print(3 == "3")
+# # print("3" > 3)
+# print("Hello" == "hello")
+
+# Exercise 4
+# computer_brand= "asus"
+# print(f"I have a {computer_brand} computer")
+
+# Exercise 5
+
+# name = "Tural"
+# age = 29
+# shoe_size = 42
+# info = f"My name is {name}, I'm {age} years old and finally my shoe size is {shoe_size}"
+# print(info)
+
+# # Exercise 6
+# a = 5
+# b = 4
+# if a>b:
+#     print("hello world")
+
+
+# Exercise 7
+# def num_checker(num):
+#     if num%2 == 0:
+#         print(f"{num} is an even number")
+#     else:
+#         print(f"{num} is an odd number")
+
+# num_checker(5)
+# num_checker(6)
+# num_checker(11)
+# num_checker(22)
+# num_checker(25)
+
+# Exercise 8
+
+# my_name = "Tural"
+
+# def check_name ():
+#     x = input("Please state your name: ")
+#     if x == my_name:
+#         print("Ha ha we both have the same name what a world ")
+#     else:
+#         print("I like your name but my name is waaay cooler ")
+
+# check_name()
+# check_name()
+# check_name()
+# check_name()
+
+# Exercise 9 
