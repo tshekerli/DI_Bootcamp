57f5bfb85af176839b13717174c182eb4c933b3a
tshekerli <tshekerli@gmail.com>
2024-03-31 21:04:45 +0400

    Update function to handle edge cases

---
+++ W19/Day2/ExerciseXpGold/main.py
@@ -0,0 +1,27 @@
+# Exercise 1
+# print("Hello world\nHello world\nHello world\nHello world\nI love python\nI love python\nI love python\nI love python\n")
+# Exercise 2
+
+
+# def seasonchecker():
+
+#     user_choice = int(
+#         input(
+#             "Please enter a number between 1 and 12 for me to tell you that month is in which season "
+#         )
+#     )
+
+#     if user_choice == 1 or user_choice == 2 or user_choice == 12:
+#         print(f"You selected {user_choice} month and that month is in winter")
+#     elif user_choice == 3 or user_choice == 4 or user_choice == 5:
+#         print(f"You selected {user_choice} month and that month is in spring")
+#     elif user_choice == 6 or user_choice == 7 or user_choice == 8:
+#         print(f"You selected {user_choice} month and that month is in summer")
+#     else:
+#         print(f"You selected {user_choice} month and that month is in fall")
+
+
+# seasonchecker()
+# seasonchecker()
+# seasonchecker()
+# seasonchecker()
