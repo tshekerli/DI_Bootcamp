bb24a859b9042ba5efd08e7e1da598e3f3459233
tshekerli <tshekerli@gmail.com>
2024-04-02 19:38:19 +0400

    Fix bug in login functionality

---
+++ W19/Day4/ExerciseXP/main.py
@@ -0,0 +1,40 @@
+# #Exercise 1
+
+# my_fav_numbers = [3, 7, 13, 21]
+# my_fav_numbers.append(2)
+# my_fav_numbers.append(5)
+# my_fav_numbers.pop(-1)
+
+# friend_fav_numbers= [1, 2, 5, 11, 21, 7]
+# our_fav_numbers = []
+
+# for x in my_fav_numbers:
+#     if x in friend_fav_numbers:
+#         our_fav_numbers.append(x)
+
+# print(our_fav_numbers)
+
+#Exercise 2
+
+# ex2_tuple = (2, 3, 4, 5)
+# ex2_tuple.append(5) #this will give an error
+# print(ex2_tuple) #it did
+
+#Exercise 3
+
+# basket = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+# basket.remove("Banana")
+# basket.remove("Blueberries")
+# basket.append("Kiwi")
+# basket.insert(0, "Apples")
+# apple_count = 0
+# for apple in basket:
+#     if apple == "Apples":
+#         apple_count+=1
+
+# print(basket)
+# print(f"apple count is {apple_count} " )
+# basket.clear()
+# print(basket)   
+
