a86bd375219db5668089b6ca1aac84122f75387d
tshekerli <tshekerli@gmail.com>
2024-04-09 19:31:57 +0400

    Add functions for full name manipulation and Morse code conversion

---
+++ W20/Day4/ExerciseXpGold/main.py
@@ -0,0 +1,26 @@
+#Exercise 1
+
+def get_full_name(first_name, last_name, middle_name=None):
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+#Exercise 2
+
+morse_code_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...',
+    ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.', ' ': '/'
+}
+
+def english_to_morse(english_text):
+    return ' '.join(morse_code_dict[i.upper()] for i in english_text)
+
+def morse_to_english(morse_code):
+    inv_morse_code_dict = {v: k for k, v in morse_code_dict.items()}
+    return ''.join(inv_morse_code_dict[i] for i in morse_code.split())
